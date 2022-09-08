c42fcdc3cf5bc4705cb12acb6adc780857d7189f
martinkaintas <martinkaintas@gmail.com>
2022-09-08 14:04:00 +0300

    step-one: initialize accounts

--- app/src/components/channel-initialization.vue
+++ app/src/components/channel-initialization.vue
@@ -1,10 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { initSdk, initiatorSdk, responderSdk } from '../utils/sdk/sdk.service';
+
+async function initializeSdk() {
+  await initSdk();
+  console.log(initiatorSdk);
+  console.log(responderSdk);
+}
+</script>
 
 <template>
   <div class="channel-initialization">
     <h1>Channel Initialization</h1>
     <p>This is the channel initialization component.</p>
-    <button disabled>Initialize Channel</button>
+    <button @click="initializeSdk()">Initialize Channel</button>
   </div>
 </template>
 
